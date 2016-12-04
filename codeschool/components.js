5d74bde97e1a97adfa95d0ce55cc3b06842a88e8
Luiz Cezer <lccezinha@gmail.com>
2016-12-04 20:11:47 -0200

    Add form comment

--- codeschool/components.js
+++ codeschool/components.js
@@ -16,13 +16,42 @@ class Comment extends React.Component {
   }
 }
 
+class CommentForm extends React.Component {
+  render() {
+    return (
+      <form className="comment-form" onSubmit={this._handleSubmit.bind(this)}>
+        <div className="comment-form-fields">
+          <input placeholder="Your name" ref={(input) => this._author = input}/><br />
+          <textarea placeholder="Your comment" ref={(textarea) => this._body = textarea}></textarea>
+        </div>
+        <div className="comment-form-actions">
+          <button type="submit">Post comment</button>
+        </div>
+      </form>
+    );
+  }
+
+  _handleSubmit(event) {
+    event.preventDefault();
+
+    var author = this._author;
+    var body = this._body;
+
+    this.props.addComment(author.value, body.value);
+  }
+}
+
 class CommentBox extends React.Component {
 
   constructor() {
     super();
 
     this.state = {
-      showComments: false
+      showComments: false,
+      comments: [
+        { id: 1, author: 'Luiz Cezer', body: 'Some comment' },
+        { id: 2, author: 'Cezer Luiz', body: 'Some commentssssssssssssssssss' }
+      ]
     };
   }
 
@@ -39,10 +68,22 @@ class CommentBox extends React.Component {
     return(
       <div className="comment-box">
         <h4 className="comment-count">{this._getCommentsTitle(comments.length)}</h4>
+        <CommentForm addComment={this._addComment.bind(this)} />
+        <hr />
         <button onClick={this._handleClick.bind(this)}>{buttonText}</button>
         {commentsList}
       </div>
     );
+  }
+
+  _addComment(author, body) {
+    var comment = {
+      id: this.state.comments.length + 1,
+      author,
+      body
+    };
+
+    this.setState({ comments: this.state.comments.concat([comment]) });
   }
 
   _handleClick() {
@@ -52,12 +93,7 @@ class CommentBox extends React.Component {
   }
 
   _getComments() {
-    var commentList = [
-      { id: 1, author: 'Luiz Cezer', body: 'Some comment' },
-      { id: 2, author: 'Cezer Luiz', body: 'Some commentssssssssssssssssss' }
-    ];
-
-    return commentList.map((comment) => {
+    return this.state.comments.map((comment) => {
       return (<Comment author={comment.author} body={comment.body} key={comment.id} />)
     });
   }
