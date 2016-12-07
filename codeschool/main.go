cbd38652d3274fc61a5012b6e891bd875233ffaa
Luiz Cezer <lccezinha@gmail.com>
2016-12-07 21:07:24 -0200

    Prepare backed to destroy comment

--- codeschool/main.go
+++ codeschool/main.go
@@ -2,9 +2,13 @@ package main
 
 import (
 	"encoding/json"
+	"errors"
 	"log"
 	"net/http"
+	"strconv"
 )
+
+var InvalidHTTPMethod = errors.New("Invalid HTTP Method for delete")
 
 type comment struct {
 	Id     int    `json:"id"`
@@ -12,29 +16,45 @@ type comment struct {
 	Body   string `json:"body"`
 }
 
-type comments []comment
+type Comments []comment
 
-func buildComments() comments {
-	return comments{
-		comment{1, "Luiz Cezer", "Some comment"},
-		comment{2, "Cezer Luiz", "Another comment"},
-	}
+var comments Comments
+
+func init() {
+	comments = append(comments, comment{1, "Luiz Cezer", "Some comment"}, comment{2, "Cezer Luiz", "Another comment"})
 }
 
-func getComments(w http.ResponseWriter, r *http.Request) {
+func indexComments(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
-
-	comments := buildComments()
 
 	if err := json.NewEncoder(w).Encode(comments); err != nil {
 		panic(err)
 	}
 }
 
+func destroyComment(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "DELETE" {
+		panic(InvalidHTTPMethod)
+	}
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+	w.WriteHeader(http.StatusNoContent)
+
+	commentId := r.URL.Query().Get("commentId")
+	commentIdInt, _ := strconv.Atoi(commentId)
+
+	for i, comment := range comments {
+		if comment.Id == commentIdInt {
+			comments = append(comments[:i], comments[i+1:]...)
+		}
+	}
+}
+
 func main() {
-	http.HandleFunc("/comments", getComments)
+	http.HandleFunc("/comments", indexComments)
+	http.HandleFunc("/comments/delete", destroyComment)
 
 	log.Println("Server running...")
 	log.Fatal(http.ListenAndServe(":8001", nil))
