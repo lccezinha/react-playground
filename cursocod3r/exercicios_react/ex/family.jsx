1c42ae5b217dd885d76232d150be8b00dbb920ea
Luiz Cezer <lccezinha@gmail.com>
2018-03-08 22:58:00 -0300

    Render family with children

--- cursocod3r/exercicios_react/ex/family.jsx
+++ cursocod3r/exercicios_react/ex/family.jsx
@@ -1,10 +1,7 @@
 import React from 'react'
-import Member from './member'
 
 export default props => (
   <div>
-    <Member name="Walter" lastName="White"></Member>
-    <Member name="Hei" lastName="senberg"></Member>
-    <Member name="Saul" lastName="Better Call"></Member>
+    {props.children}
   </div>
 )
--- cursocod3r/exercicios_react/ex/index.jsx
+++ cursocod3r/exercicios_react/ex/index.jsx
@@ -1,5 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import Member from './member'
 import Family from './family'
 
-ReactDOM.render(<Family />, document.getElementById('app'))
+ReactDOM.render(
+  <Family>
+    <Member name="Walter" lastName="White #1"></Member>
+    <Member name="Walter" lastName="White #2"></Member>
+    <Member name="Walter" lastName="White #3"></Member>
+  </Family>,
+document.getElementById('app'))
