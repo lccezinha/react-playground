539da0088a61a46aff88e8e1b78459dc149980d7
Luiz Cezer <lccezinha@gmail.com>
2018-03-20 20:47:35 -0300

    Copy files

--- cursocod3r/exercicios_react/ex/family.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { childreWithProps } from '../utils/reactUtils'
-
-export default props => (
-  <div>
-    <h1>Família</h1>
-    { childreWithProps(props.children, props) }
-  </div>
-)
