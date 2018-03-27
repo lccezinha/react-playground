d62ebf2fd18a6ef124a469131eeeec573ba6e7c7
Luiz Cezer <lccezinha@gmail.com>
2018-03-27 20:25:35 -0300

    Copy last lesson

--- cursocod3r/exercicios_react/ex/index.jsx
+++ cursocod3r/exercicios_react/ex/index.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import MyComponent from './classComponent'
 
-ReactDOM.render(<MyComponent label="Count" initialValue={1} />,document.getElementById('app'))
+ReactDOM.render(,document.getElementById('app'))
