cfa445e73eaa75d881a42310968f9b7ea1912cc9
Luiz Cezer <lccezinha@gmail.com>
2018-04-03 20:48:46 -0300

    Setup app todo

---
+++ cursocod3r/exercicios_react/ex5/index.jsx
@@ -0,0 +1,5 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import MyComponent from './classComponent'
+
+ReactDOM.render(<MyComponent label="Count" initialValue={1} />,document.getElementById('app'))
