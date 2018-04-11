09cc3be02250600ca3f31dd85793ef2902351ed6
Luiz Cezer <lccezinha@gmail.com>
2018-04-10 21:40:30 -0300

    Finish backend app

--- cursocod3r/todo-app/backend/src/config/server.js
+++ cursocod3r/todo-app/backend/src/config/server.js
@@ -1,10 +1,12 @@
 const bodyParser = require('body-parser')
 const express = require('express')
 const server = express()
+const allowCors = require('./cors')
 const port = 3003
 
 server.use(bodyParser.urlencoded({ extended: true }))
 server.use(bodyParser.json())
+server.use(allowCors)
 
 server.listen(port, function() {
   console.log(`Backend running on port ${port}`)
