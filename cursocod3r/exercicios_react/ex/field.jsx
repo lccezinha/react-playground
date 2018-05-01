af0f901a5b4ae04fe330b59e169d5ffdfa4a169d
Luiz Cezer <lccezinha@gmail.com>
2018-05-01 19:29:29 -0300

    Create fieldAction and Reducer

--- cursocod3r/exercicios_react/ex/field.jsx
+++ cursocod3r/exercicios_react/ex/field.jsx
@@ -1,12 +1,14 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+import { changeValue } from './fieldActions'
 
 class Field extends Component {
   render() {
     return(
       <div>
-        <label>{this.props.my_value}</label>
-        <input onChange={this.handleChange} value={this.props.my_value} />
+        <label>{this.props.value}</label>
+        <input onChange={this.props.changeValue} value={this.props.value} />
       </div>
     )
   }
@@ -14,8 +16,12 @@ class Field extends Component {
 
 function mapStateToProps(state) {
   return {
-    my_value: state.field.value
+    value: state.field.value
   }
 }
 
-export default connect(mapStateToProps)(Field)
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({ changeValue }, dispatch)
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Field)
